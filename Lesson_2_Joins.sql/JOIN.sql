75097dd7e355afdfd7633296fc144efeaf8d30cd
Hassanat Oluwatobi Awodipe <45914807+Hassanat-Awodipe@users.noreply.github.com>
2022-04-01 12:02:55 +0200

    added comments

--- Lesson_2_Joins.sql/JOIN.sql
+++ Lesson_2_Joins.sql/JOIN.sql
@@ -1,17 +1,17 @@
-1.) Try pulling all the data from the accounts table, and all the data from the orders table
+--1.) Try pulling all the data from the accounts table, and all the data from the orders table
 
 SELECT *
 FROM accounts
 JOIN orders
 ON accounts.id = orders.id
 
-2.) Try pulling standard_qty, gloss_qty, and poster_qty from the orders table, and the website 
-and the primary_poc from the accounts table.
+/*2.) Try pulling standard_qty, gloss_qty, and poster_qty from the orders table, and the website
+and the primary_poc from the accounts table.*/
 
 SELECT accounts.name,
-	orders.standard_qty, 
-	orders.gloss_qty, 
-    	orders.poster_qty, 
+			orders.standard_qty,
+			orders.gloss_qty, 
+    	orders.poster_qty,
     	accounts.website,
     	accounts.primary_poc
 FROM orders
