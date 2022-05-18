4581617d0872f1fa76f352e2982ad8b998d95709
Hassanat Oluwatobi Awodipe <45914807+Hassanat-Awodipe@users.noreply.github.com>
2022-05-18 22:45:10 +0200

    Update NOT_IN.sql

--- Lesson_1_Basics.sql/NOT_IN.sql
+++ Lesson_1_Basics.sql/NOT_IN.sql
@@ -6,7 +6,9 @@ concepts with our new operator.
 1.) Use the accounts table to find the account name, primary poc, and sales rep id for all
 stores except Walmart, Target, and Nordstrom.*/
 
-SELECT name, primary_poc, sales_rep_id
+SELECT name, 
+      primary_poc,
+      sales_rep_id
 FROM accounts
 WHERE name NOT IN ('Walmart', 'Target', 'Nordstrom');
 
