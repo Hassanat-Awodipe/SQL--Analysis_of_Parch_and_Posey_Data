68c6c890942c79871c6ee3f6a813b33e7bf1ee5c
Hassanat Oluwatobi Awodipe <45914807+Hassanat-Awodipe@users.noreply.github.com>
2022-05-18 22:46:56 +0200

    Update where_query.sql

--- Lesson_1_Basics.sql/where_query.sql
+++ Lesson_1_Basics.sql/where_query.sql
@@ -18,6 +18,8 @@ LIMIT 10;
 /*3.) Filter the accounts table to include the company name, website, and the primary point of
 contact (primary_poc) just for the Exxon Mobil company in the accounts table.*/
 
-SELECT name, website, primary_poc
+SELECT name, 
+        website, 
+        primary_poc
 FROM accounts
 WHERE name = 'Exxon Mobil';
