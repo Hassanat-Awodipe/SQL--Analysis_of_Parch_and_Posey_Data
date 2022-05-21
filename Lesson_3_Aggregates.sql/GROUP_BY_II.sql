67b51732de5b42e58ae2b24ce6fdbcd7e1d9a0c5
Hassanat Oluwatobi Awodipe <45914807+Hassanat-Awodipe@users.noreply.github.com>
2022-05-21 21:54:05 +0200

    Update GROUP_BY_II.sql

--- Lesson_3_Aggregates.sql/GROUP_BY_II.sql
+++ Lesson_3_Aggregates.sql/GROUP_BY_II.sql
@@ -1,4 +1,4 @@
-1.) For each account, determine the average amount of each type of paper they purchased across their orders. Your result should have four columns - one for the account name and one for the average quantity purchased for each of the paper types for each account.
+--1.) For each account, determine the average amount of each type of paper they purchased across their orders. Your result should have four columns - one for the account name and one for the average quantity purchased for each of the paper types for each account.
 
 SELECT a.name, 
 	AVG(standard_qty) ave_stan, 
@@ -9,7 +9,7 @@ JOIN orders o
 ON o.account_id = a.id
 GROUP BY a.name
 
-2.) For each account, determine the average amount spent per order on each paper type. Your result should have four columns - one for the account name and one for the average amount spent on each paper type.
+--2.) For each account, determine the average amount spent per order on each paper type. Your result should have four columns - one for the account name and one for the average amount spent on each paper type.
 
 SELECT a.name, 
 	AVG(standard_amt_usd) ave_stan, 
@@ -20,7 +20,7 @@ JOIN orders o
 ON o.account_id = a.id
 GROUP BY a.name
 
-3.) Determine the number of times a particular channel was used in the web_events table for each sales rep. Your final table should have three columns - the name of the sales rep, the channel, and the number of occurrences. Order your table with the highest number of occurrences first.
+--3.) Determine the number of times a particular channel was used in the web_events table for each sales rep. Your final table should have three columns - the name of the sales rep, the channel, and the number of occurrences. Order your table with the highest number of occurrences first.
 
 SELECT s.name,
 	w.channel,
@@ -33,7 +33,7 @@ ON s.id = a.sales_rep_id
 GROUP BY s.name, w.channel
 ORDER BY num_of_occu DESC
 
-4.) Determine the number of times a particular channel was used in the web_events table for each region. Your final table should have three columns - the region name, the channel, and the number of occurrences. Order your table with the highest number of occurrences first.
+--4.) Determine the number of times a particular channel was used in the web_events table for each region. Your final table should have three columns - the region name, the channel, and the number of occurrences. Order your table with the highest number of occurrences first.
 
 SELECT r.name,
 	w.channel,
