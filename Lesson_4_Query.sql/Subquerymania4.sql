8078bf63a5a29478cbb78aa2e500a64db35c3919
Hassanat Oluwatobi Awodipe <45914807+Hassanat-Awodipe@users.noreply.github.com>
2022-05-29 14:29:11 +0200

    Update Subquerymania4.sql

--- Lesson_4_Query.sql/Subquerymania4.sql
+++ Lesson_4_Query.sql/Subquerymania4.sql
@@ -3,7 +3,7 @@
 
 get the total_amt_usd for all customers*/
 SELECT a.name acc_name,
-	     SUM(o.total_amt_usd) total_sales
+	SUM(o.total_amt_usd) total_sales
 FROM orders o
 JOIN accounts a
 ON a.id = o.account_id
@@ -11,7 +11,7 @@ GROUP BY 1
 
 --get the highest of them
 SELECT a.name acc_name,
-	     SUM(o.total_amt_usd) total_sales,
+	SUM(o.total_amt_usd) total_sales,
 FROM orders o
 JOIN accounts a
 ON a.id = o.account_id
@@ -26,12 +26,11 @@ FROM web_events w
 JOIN accounts a
 ON w.account_id = a.id AND a.name = (SELECT acc_name
                                       FROM (SELECT a.name acc_name,
-	                                               SUM(o.total_amt_usd) total_sales
+	                                            SUM(o.total_amt_usd) total_sales
                                             FROM orders o
                                             JOIN accounts a
                                             ON a.id = o.account_id
                                             GROUP BY 1
-                                            ORDER BY 2 DESC
                                             LIMIT 1) t1
                                       )
 
@@ -43,12 +42,11 @@ FROM web_events w
 JOIN accounts a
 ON w.account_id = a.id AND a.name = (SELECT acc_name
                                       FROM (SELECT a.name acc_name,
-	                                               SUM(o.total_amt_usd) total_sales
+	                                            SUM(o.total_amt_usd) total_sales
                                             FROM orders o
                                             JOIN accounts a
                                             ON a.id = o.account_id
                                             GROUP BY 1
-                                            ORDER BY 2 DESC
                                             LIMIT 1) t1
                                       )
   GROUP BY 1,3
@@ -75,7 +73,6 @@ FROM (SELECT a.name acc_name,
       JOIN accounts a
       ON a.id = o.account_id
       GROUP BY 1
-      ORDER BY 2 DESC
       LIMIT 10) t1
 GROUP BY 1
 
@@ -86,7 +83,6 @@ FROM (SELECT a.id, a.name, SUM(o.total_amt_usd) tot_spent
       JOIN accounts a
       ON a.id = o.account_id
       GROUP BY a.id, a.name
-      ORDER BY 3 DESC
        LIMIT 10) temp;
 
 /*6.) What is the lifetime average amount spent in terms of total_amt_usd, including
