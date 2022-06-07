f5a709662596f6ccf90c94ca33d6dc3e197c5f5a
Hassanat Oluwatobi Awodipe <45914807+Hassanat-Awodipe@users.noreply.github.com>
2022-06-07 15:14:36 +0200

    Update UNION_example.sql

--- Lesson_7_AdvancedJoins.sql/UNION_example.sql
+++ Lesson_7_AdvancedJoins.sql/UNION_example.sql
@@ -1,28 +1,29 @@
 --Example
 SELECT channel,
-		COUNT(*) AS sessions
+	COUNT(*) AS sessions
 FROM (
-		SELECT *
-		FROM web_events
-	
-		UNION ALL
-	
-		SELECT *
-		FROM web_events
+	SELECT *
+	FROM web_events
+
+	UNION ALL
+
+	SELECT *
+	FROM web_events
 	 )all_events
 GROUP BY 1
 ORDER BY 2 DESC;
 
+
 WITH all_events AS (SELECT *
-				   FROM web_events
-				   
-				   UNION ALL
-				  
-					SELECT *
-				   	FROM web_events)
+		   FROM web_events
+
+		   UNION ALL
+
+		   SELECT *
+		   FROM web_events)
 					
 SELECT channel,
-		COUNT(*) AS sessions
+	COUNT(*) AS sessions
 FROM all_events
 GROUP BY 1
 ORDER BY 2 DESC;
