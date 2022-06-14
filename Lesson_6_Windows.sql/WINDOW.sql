ff7d32a2a14a926e8e2eb95ff5e82bbaddca45ba
Hassanat Oluwatobi Awodipe <45914807+Hassanat-Awodipe@users.noreply.github.com>
2022-06-14 20:20:57 +0200

    Update WINDOW.sql

--- Lesson_6_Windows.sql/WINDOW.sql
+++ Lesson_6_Windows.sql/WINDOW.sql
@@ -2,23 +2,23 @@
 
 
 SELECT standard_qty,
-		SUM(standard_qty) OVER (ORDER BY occurred_at) AS running_total
-FROM orders
+	SUM(standard_qty) OVER (ORDER BY occurred_at) AS running_total
+FROM orders;
 
 
 SELECT standard_qty,
-		DATE_TRUNC('month', occurred_at) AS month,
-		SUM(standard_qty) OVER (ORDER BY occurred_at) AS running_total
-FROM orders
+	DATE_TRUNC('month', occurred_at) AS month,
+	SUM(standard_qty) OVER (ORDER BY occurred_at) AS running_total
+FROM orders;
 
 
 SELECT standard_qty,
-		DATE_TRUNC('month', occurred_at) AS month,
-		SUM(standard_qty) OVER (PARTITION BY DATE_TRUNC('month', occurred_at) ORDER BY occurred_at) AS running_total
-FROM orders
+	DATE_TRUNC('month', occurred_at) AS month,
+	SUM(standard_qty) OVER (PARTITION BY DATE_TRUNC('month', occurred_at) ORDER BY occurred_at) AS running_total
+FROM orders;
 
 
 SELECT standard_qty,
-		DATE_TRUNC('month', occurred_at) AS month,
-		SUM(standard_qty) OVER (PARTITION BY DATE_TRUNC('month', occurred_at)) AS running_total
-FROM orders
+	DATE_TRUNC('month', occurred_at) AS month,
+	SUM(standard_qty) OVER (PARTITION BY DATE_TRUNC('month', occurred_at)) AS running_total
+FROM orders;
