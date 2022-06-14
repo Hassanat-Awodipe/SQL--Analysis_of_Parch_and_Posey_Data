24df3277801e6e26086bdcdcf0e29c3d4dc7e666
Hassanat Oluwatobi Awodipe <45914807+Hassanat-Awodipe@users.noreply.github.com>
2022-06-14 20:22:17 +0200

    Update PARTITION_BY.sql

--- Lesson_6_Windows.sql/PARTITION_BY.sql
+++ Lesson_6_Windows.sql/PARTITION_BY.sql
@@ -1,13 +1,13 @@
 SELECT standard_amt_usd,
-		DATE_TRUNC('month', occurred_at) AS month,
-		SUM(standard_amt_usd) OVER (ORDER BY occurred_at) AS running_total
+	DATE_TRUNC('month', occurred_at) AS month,
+	SUM(standard_amt_usd) OVER (ORDER BY occurred_at) AS running_total
 FROM orders;
 
 /*1. Create a running total of standard_amt_usd (in the orders table) over order time with no date truncation. Your final
 table should have two columns: one with the amount being added for each new row, and a second with the running total.*/
 
 SELECT standard_amt_usd,
-		SUM(standard_amt_usd) OVER (ORDER BY occurred_at) AS running_total
+	SUM(standard_amt_usd) OVER (ORDER BY occurred_at) AS running_total
 FROM orders;
 
 /*2. Now, modify your query from the previous quiz to include partitions. Still create a running total of standard_amt_usd
@@ -16,8 +16,8 @@ occurred_at variable. Your final table should have three columns: One with the a
 truncated date, and a final column with the running total within each year. */
 
 SELECT standard_amt_usd,
-		DATE_TRUNC('year', occurred_at) AS year,
-		SUM(standard_amt_usd) OVER (PARTITION BY DATE_TRUNC('year', occurred_at) ORDER BY occurred_at) AS running_total
+	DATE_TRUNC('year', occurred_at) AS year,
+	SUM(standard_amt_usd) OVER (PARTITION BY DATE_TRUNC('year', occurred_at) ORDER BY occurred_at) AS running_total
 FROM orders;
 
 
